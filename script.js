1b8bbdc3e1d46fba73587e59570eed2d9613e5ab
Eric <49057152+McQueen5258@users.noreply.github.com>
2020-04-05 18:45:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+$(".garage-door-opener").click(function() {
+    $(".garage-door").slideToggle();
+});
+
+$(".pokeball").click(function() {
+    $(".pikachu").fadeToggle();
+});
+
+$(".light-switch").click(function() {
+    $(".light-bulb").toggle();
+});
